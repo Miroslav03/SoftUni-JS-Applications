c94e360982e834a09bb64436233e73d7f9131600
Miroslav Ivanov <znaka5@abv.bg>
2023-11-18 23:37:37 +0200

    tests

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-const obj = {
-    name:'wefasf',
-    high:8,
-    low:2
-}
+async function solution() {
+    const url1 =  fetch('http://localhost:3030/jsonstore/advanced/articles/list')
+    const url2 = fetch('http://localhost:3030/jsonstore/advanced/articles/details')
+    const [username,text] = await Promise.all([(await url1).json(),(await url2).json()])
 
-console.log(Object.entries(obj));
+    console.log(username,text);
+}
+solution()    
