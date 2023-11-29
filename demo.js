20bc732a3a14b3beeb0106a39d3dddf9e3d9e764
Miroslav Ivanov <znaka5@abv.bg>
2023-11-30 01:26:53 +0200

    test

---
+++ demo.js
@@ -0,0 +1,23 @@
+function sovle(arr) {
+
+    for (let i = 3; i <= arr.length; i+=2) {
+        const element = arr[i];
+        let a
+        
+    }
+
+} sovle(["Sandra Bullock",
+"340",
+"5",
+"Robert De Niro",
+"50",
+"Julia Roberts",
+"40.5",
+"Daniel Day-Lewis",
+"39.4",
+"Nicolas Cage",
+"29.9",
+"Stoyanka Mutafova",
+"33"]
+
+)
